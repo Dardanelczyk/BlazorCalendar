41a3296e82bcb55400f9c414d36f38237033a614
Dardanelczyk <dardanelczyk@gmail.com>
2020-07-13 15:48:31 +0200

    Add new files

---
+++ BlazorCalendar/Models/CalendarDay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCalendar.Models
+{
+    public class CalendarDay
+    {
+        public int DayNumber { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsEmpty { get; set; }
+
+        public List<CalendarEvent> Events { get; set; }
+    }
+}
---
+++ BlazorCalendar/Services/ICalendarEventsProvider.cs
@@ -0,0 +1,15 @@
+﻿using BlazorCalendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCalendar.Services
+{
+    public interface ICalendarEventsProvider
+    {
+        Task<IEnumerable<CalendarEvent>> GetEventsInMonthAsync(int year, int month);
+
+        Task AddEventAsync(CalendarEvent calendarEvent);
+    }
+}
